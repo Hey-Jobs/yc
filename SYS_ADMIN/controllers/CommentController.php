9e03922e94ce4eec5deb527759b14331612ecea4
许松涛 <287887940@qq.com>
2018-10-20 16:19:55 +0800

    评论。

---
+++ SYS_ADMIN/controllers/CommentController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wilbur
+ * Date: 2018/10/20
+ * Time: 11:51
+ */
+
+namespace SYS_ADMIN\controllers;
+
+
+class CommentController extends CommonController
+{
+    public function actionIndex()
+    {
+        echo 'Comment';
+        exit;
+    }
+}
