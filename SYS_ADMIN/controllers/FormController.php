caaf1790b852c20ad235eece1191dc318cd04d14
xusongtao <songtao.xu@zhenai.com>
2018-11-08 17:14:55 +0800

    Form--LiveRoom.

--- SYS_ADMIN/controllers/FormController.php
+++ SYS_ADMIN/controllers/FormController.php
@@ -1,19 +1,14 @@
 <?php
 namespace SYS_ADMIN\controllers;
 
+use SYS_ADMIN\components\BaseDataBuilder;
 use SYS_ADMIN\models\LiveRoom;
 
 class FormController extends CommonController
 {
     public function actionRoom()
     {
-        $liveList = \SYS_ADMIN\models\LiveRoom::find()
-            ->select(
-                ['room_name as text',
-                'id']
-            )
-            ->asArray()
-            ->all();
+        $liveList = BaseDataBuilder::instance('LiveRoom', false);
         return $this->successInfo($liveList);
     }
 }
