7431b30ee805511dc0a0e204355634b70ee94da5
xusongtao <songtao.xu@zhenai.com>
2018-11-14 11:50:51 +0800

    test.

--- SYS_ADMIN/controllers/TextController.php
+++ SYS_ADMIN/controllers/TextController.php
@@ -1,10 +1,51 @@
 <?php
 namespace SYS_ADMIN\controllers;
 
-use SYS_ADMIN\models\SysDemo;
+use app\models\Test;
+use yii\web\Controller;
 
-class TextController extends CommonController
+class TextController extends \yii\rest\Controller
 {
+    /**
+     * 返回错误信息
+     * {
+     *   error: "请求参数有误。",
+     *   message: "日志ID不能为空。",
+     *   code: 4001,
+     *   status: 400
+     * }
+     */
+    public function errorInfo($code, $extend_message = '')
+    {
+        $route = $this->action->controller->module->requestedRoute ?? '';
+        $result = [
+            'timestamp' => time(),
+            'status' => 400,
+            'error' => $code,
+            'message' => is_array($extend_message) ? join("\n", $extend_message) : $extend_message,
+            'code' => $code,
+            'path' => $route,
+        ];
+        echo json_encode($result);
+        exit;
+    }
+
+    /**
+     * 返回成功的信息
+     * @param  array $data 返回的数据
+     * @return array
+     */
+    public function successInfo($data = [])
+    {
+        $result = [
+            'timestamp' => time(),
+            'status' => 200,
+            'data' => $data,
+        ];
+        echo json_encode($result);
+        exit;
+    }
+
     /**
      * Site Index
      */
@@ -14,5 +55,31 @@ class TextController extends CommonController
         return $this->render('index', [
             'model' => $model
         ]);
+    }
+
+    public function actionAdd()
+    {
+        $content = \Yii::$app->request->post('content');
+
+        $testM = new Test();
+        $testM->content = $content;
+        if (!$testM->save()) {
+            return $this->errorInfo(400);
+        }
+
+        return $this->successInfo(true);
+    }
+
+    public function actionOne()
+    {
+        $testInfo = Test::find()
+            ->limit(3)
+            ->asArray()
+            ->all();
+
+//        foreach ($testInfo as $key => $row) {
+//            $testInfo[$key]['content'] = json_decode($row['content']);
+//        }
+        return $this->successInfo($testInfo);
     }
 }
