2c8ad42ea5e92ca6d865b6e518b4d235795f5252
jianxun <1715661215@qq.com>
2018-12-04 00:48:43 +0800

    镜头封面图

--- SYS_ADMIN/controllers/rest/v1/WechatController.php
+++ SYS_ADMIN/controllers/rest/v1/WechatController.php
@@ -194,6 +194,7 @@ class WechatController extends CommonController
             'keyword5' => $order_info->user_name ." ".$order_info->user_phone." ".$order_info->user_address,
         ];
         if($client_info->open_id){
+            $msg_data['first'] = "客户下单成功，请尽快安排配送服务";
             $result = $template->send($client_info->open_id, $template_id['order_success'], $notify_url,$msg_data);
             CommonHelper::writeOrderLog(['type' => 'send template msg', 'data' => $result]);
         }
