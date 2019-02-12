0a4951fa25c0a8f918ab31cde52ab11fbd90fb97
jianxun <1715661215@qq.com>
2019-02-12 22:15:08 +0800

    微信消息管理

--- SYS_ADMIN/controllers/rest/v1/CommonController.php
+++ SYS_ADMIN/controllers/rest/v1/CommonController.php
@@ -27,7 +27,7 @@ class CommonController extends \yii\rest\Controller
         $base_url = \Yii::$app->request->getPathInfo();
         $action_name = str_replace('rest/v1/wechat/', '', $base_url);
         $action_name = trim($action_name, '/');
-        $no_auth = ['jssdk', 'auth-login', 'notify', 'auth-login-back'];
+        $no_auth = ['jssdk', 'auth-login', 'notify', 'auth-login-back', 'add-menu', 'message'];
 
         if (1 == $debug) { //调式模式
             $open_id = 'o5NW-52_GfBdOhc4nm2-Ggtardkg';
--- SYS_ADMIN/controllers/rest/v1/WechatController.php
+++ SYS_ADMIN/controllers/rest/v1/WechatController.php
@@ -338,4 +338,38 @@ class WechatController extends CommonController
         $sdk = $js->buildConfigJs($apis, false, $url);
         return $this->successInfo($sdk);
     }
+
+
+    /**
+     * 添加菜单栏
+     */
+    public function actionAddMenu()
+    {
+        $conf = \Yii::$app->params['wx']['mp'];
+        $menu = (new Application(['conf' => $conf]))->driver("mp.menu");
+        $buttons = [
+            'button' => [
+                [
+                    'type' => 'click',
+                    'name' => '溯源直播',
+                    'url' => 'https://yc.adaxiang.com/front/?from=singlemessage#/',
+                ],
+            ]
+        ];
+        $result = $menu->create($buttons);
+        var_dump($result);
+    }
+
+    /**
+     * 接收微信消息
+     */
+    public function actionMessage()
+    {
+        $conf = \Yii::$app->params['wx']['mp'];
+        $server = (new Application(['conf' => $conf]))->driver("mp.server");
+        $server->setMessageHandler(function($message) {
+            file_put_contents('wx.txt', json_encode($message), FILE_APPEND);
+            return "欢迎你";
+        });
+    }
 }
