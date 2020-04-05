8b113c2a1aa67903d678fff98231ae14cb5bdd7e
jianxun <1715661215@qq.com>
2020-04-05 21:29:13 +0800

    修改

--- SYS_ADMIN/components/Wechat.php
+++ SYS_ADMIN/components/Wechat.php
@@ -14,9 +14,6 @@ namespace SYS_ADMIN\components;
  */
 class Wechat
 {
-    static $APPID = "wx6813aaf9a23e0aaa"; // 公众号ID
-    static $APPSECRET = "d8a6f5c0b285e1819bd45f6c72494ee8"; //appsecret
-    static $KEY = "9p8JjbfSnQZIDVrdU1iO2tcGRasuymHv"; // 微信支付key
 
     static $MID = "1519888171"; // 商家ID
 }
